c113c5926c80a6b0d5bc45d4b32248d11037bdc5
GabbasovDinar <gabbasov@it-projects.info>
2015-10-22 14:02:44 +0500

    Описание таблиц
    Файл с описанием таблиц

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class User(models.Model):
     Name = models.CharField(max_length=100)
     Email = models.EmailField()
@@ -11,7 +10,7 @@ class Project(models.Model):
     NameProject = models.CharField(max_length=100)
     Client = models.CharField(max_length=100)
     def __str__(self): 
-        return self.NameProject
+        return self.NameProject 
     
 class ProjectUser(models.Model):
     UserID = models.ForeignKey('User')
