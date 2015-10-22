51e1301648f65974f1430ac355241212263ebcc5
GabbasovDinar <gabbasov@it-projects.info>
2015-10-22 12:06:58 +0500

    Модель БД
    Изминение

--- models.py
+++ models.py
@@ -1,32 +1,27 @@
 from django.db import models
 
-class Programmer(models.Model):
-    NameProgrammer = models.CharField(max_length=100)
-    EmailProgrammer = models.EmailField()
+
+class User(models.Model):
+    Name = models.CharField(max_length=100)
+    Email = models.EmailField()
+    Position = models.CharField(max_length=100)
     def __str__(self): 
-        return self.NameProgrammer 
-    
-class Manager(models.Model):
-    NameManager = models.CharField(max_length=100)
-    EmailManager = models.EmailField()
-    def __str__(self): 
-        return self.NameManager   
-       
+            return self.Name
+        
 class Project(models.Model):
     NameProject = models.CharField(max_length=100)
     Client = models.CharField(max_length=100)
-    
-    ManagerID = models.ForeignKey('Manager')
+    #if user position manager, then
+    ManagerID = models.ForeignKey('User')
     def __str__(self): 
         return self.NameProject
-class Project_line(models.Model):
-    ProjectID = models.ForeignKey('Project')
-    TasksID = models.ForeignKey('Tasks')
-
-class Tasks(models.Model):
-    Description = models.CharField(max_length=100)
-    TimeWork = models.FloatField(max_length=100)
     
-    ProgrammerID = models.ForeignKey('Programmer')
+class Tasks(models.Model):
+    DateWork = models.DateTimeField('date work')
+    TimeWork = models.FloatField(max_length=100)
+    Description = models.CharField(max_length=500)
+    #if user position programmer, then
+    ProgrammerID = models.ForeignKey('User')
+    ProjectID = models.ForeignKey('Project')
     def __str__(self): 
-        return self.Description  
+        return self.Description      
